d284003704d712e28bbc50570c6bd8eba00dc9ba
codewithayana <codewithayana@gmail.com>
2025-12-07 16:07:43 +0530

    feat:user auth routes and definitions added

--- server.js
+++ server.js
@@ -1,3 +1,33 @@
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+import helmet from "helmet"
+import morgan from "morgan"
+import { connect } from "mongoose"
+import connectDB from "./config/db.js"
+import authRoutes from "./routes/authRoutes.js"
+import userRoutes from "./routes/userRoutes.js"
 
 
+dotenv.config();
+
+const app = express();
+
+const PORT  = process.env.PORT || 3000;
+
+app.use(express.json({limit:"30mb",extented: true}));
+app.use(express.urlencoded({limit:"30mb",extended:true}))
+
+app.use(morgan("dev"))
+
+await connectDB()
+
+app.use("/api/auth", authRoutes)
+app.use("/api/users",userRoutes)
+
+app.listen(PORT, () => {
+    console.log(
+        `process ID ${process.pid}:server running on http://localhost:${PORT} in ${process.env.NODE_ENV} mode`
+    )
+})
 
